bb9d50e5c65490f68b4110ac458caad4bc949a69
Naveenkumar67 <64203146+Naveenkumar67@users.noreply.github.com>
2021-02-07 13:14:52 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -37,7 +37,7 @@ const App = ()=> {
             default:
                 setPosi({
                     left:ballPosition.left,
-                    top:ballPosition.top;
+                    top:ballPosition.top,
                 });
                 break;           
 
